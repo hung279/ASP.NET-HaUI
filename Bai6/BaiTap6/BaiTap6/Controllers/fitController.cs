089356a7fa649640773f7dd1250d6db85b00f430
hoonndev <hoonn2k2@gmail.com>
2023-04-16 23:18:31 +0700

    complete Bai6

---
+++ Bai6/BaiTap6/BaiTap6/Controllers/fitController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap6.Controllers
+{
+    public class fitController : Controller
+    {
+        // GET: fit
+        public ActionResult kythuat()
+        {
+            return View();
+        }
+
+        public ActionResult hethong()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bai6/BaiTap6/BaiTap6/Controllers/hauiController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap6.Controllers
+{
+    public class hauiController : Controller
+    {
+        // GET: haui
+        public ActionResult GioiThieu()
+        {
+            return View();
+        }
+
+        public ActionResult TuyenSinh()
+        {
+            return View();
+        }
+        public ActionResult DaoTao()
+        {
+            return View();
+        }
+    }
+}
